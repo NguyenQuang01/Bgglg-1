e8cfd6c4b673db810f9ed959f071f59f7adfede8
mh_anh.phamduc <Ducanh123>
2023-03-13 21:46:01 +0700

    api

---
+++ src/main/java/com/example/itspower/controller/ReasonController.java
@@ -0,0 +1,19 @@
+package com.example.itspower.controller;
+
+import com.example.itspower.service.ReasonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReasonController {
+    @Autowired
+    private ReasonService reasonService;
+
+    @GetMapping("/reason")
+    public ResponseEntity<Object> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(reasonService.getReason());
+    }
+}
