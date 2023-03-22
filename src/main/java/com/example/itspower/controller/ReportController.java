e11c787dca1781e1295f864bf847cd2f15ea6054
AnhPD <Ducanh123>
2023-03-22 21:24:36 +0700

    api report

--- src/main/java/com/example/itspower/controller/ReportController.java
+++ src/main/java/com/example/itspower/controller/ReportController.java
@@ -17,14 +17,11 @@ public class ReportController {
     public ResponseEntity<Object> report(@RequestParam("reportDate") String reportDate, @RequestParam("groupId") int groupId) {
         return ResponseEntity.status(HttpStatus.OK).body(reportService.reportDto(reportDate, groupId));
     }
-
-
     @PostMapping("/report/save")
     @CrossOrigin
     public ResponseEntity<Object> save(@RequestBody ReportRequest reportRequest, @RequestParam("groupId") int groupId) {
         return ResponseEntity.status(HttpStatus.OK).body(reportService.save(reportRequest, groupId));
     }
-
     @PostMapping("/report/update")
     @CrossOrigin
     public ResponseEntity<Object> update(@RequestBody ReportRequest reportRequest, @RequestParam("groupId") int groupId) {
