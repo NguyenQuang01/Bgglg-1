29cafa185f09c8958aa60dd066ec47928c8433d1
Minh <minhpd1@hblab.vn>
2023-03-23 14:24:37 +0700

    api CRUD reason

--- src/main/java/com/example/itspower/model/entity/ReasonEntity.java
+++ src/main/java/com/example/itspower/model/entity/ReasonEntity.java
@@ -1,5 +1,6 @@
 package com.example.itspower.model.entity;
 
+import com.example.itspower.request.ReasonRequest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,4 +20,11 @@ public class ReasonEntity {
     private Integer id;
     @Column(name = "name")
     private String name;
+    public ReasonEntity(ReasonRequest reasonRequest){
+        this.name= reasonRequest.getName();
+    }
+    public ReasonEntity(ReasonRequest reasonRequest,int id){
+        this.id=id;
+        this.name= reasonRequest.getName();
+    }
 }
--- src/main/java/com/example/itspower/service/ReasonService.java
+++ src/main/java/com/example/itspower/service/ReasonService.java
@@ -1,10 +1,19 @@
 package com.example.itspower.service;
 
 import com.example.itspower.model.entity.ReasonEntity;
+import com.example.itspower.request.ReasonRequest;
 
 import java.util.List;
 
 
 public interface ReasonService {
     List<ReasonEntity> searchALl();
+
+    List<ReasonEntity> save(List<ReasonRequest> reasonRequest);
+
+    ReasonEntity edit(ReasonRequest reasonRequest,int id);
+
+    void deleteAll();
+
+    void deleteById(int id);
 }
