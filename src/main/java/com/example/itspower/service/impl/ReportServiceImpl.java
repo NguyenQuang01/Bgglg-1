5808d962e982a2534de8b08b9a3a18ef93d2781e
AnhPD <Ducanh123>
2023-03-24 23:18:50 +0700

    api report

--- src/main/java/com/example/itspower/service/impl/ReportServiceImpl.java
+++ src/main/java/com/example/itspower/service/impl/ReportServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.itspower.service.impl;
 
 import com.example.itspower.component.util.DateUtils;
-import com.example.itspower.exception.ResourceNotFoundException;
 import com.example.itspower.model.entity.ReportEntity;
 import com.example.itspower.model.entity.RiceEntity;
 import com.example.itspower.model.entity.TransferEntity;
@@ -13,12 +12,12 @@ import com.example.itspower.repository.RiceRepository;
 import com.example.itspower.repository.TransferRepository;
 import com.example.itspower.request.ReportRequest;
 import com.example.itspower.response.ReportResponse;
+import com.example.itspower.response.SuccessResponse;
 import com.example.itspower.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -35,11 +34,10 @@ public class ReportServiceImpl implements ReportService {
     private RiceRepository riceRepository;
 
     @Override
-    @Transactional
-    public ReportResponse reportDto(String reportDate, int groupId) {
+    public Object reportDto(String reportDate, int groupId) {
         Optional<ReportEntity> entity = reportRepository.findByReportDateAndGroupId(reportDate, groupId);
         if (entity.isEmpty()) {
-            throw new ResourceNotFoundException(HttpStatus.BAD_REQUEST.value(), "", HttpStatus.BAD_REQUEST.name());
+            return new SuccessResponse(HttpStatus.BAD_REQUEST.value(), "", HttpStatus.BAD_REQUEST.name());
         }
         ReportDto reportDto = reportRepository.reportDto(reportDate, groupId);
         List<RestDto> restDtos = restRepository.getRests(reportDto.getId());
@@ -49,33 +47,39 @@ public class ReportServiceImpl implements ReportService {
     }
 
     @Override
-    @Transactional
-    public ReportResponse save(ReportRequest request, int groupId) {
-        Optional<ReportEntity> entity = reportRepository.findByReportDateAndGroupId(DateUtils.formatDate(new Date()), groupId);
-        if (entity.isPresent()) {
-            throw new ResourceNotFoundException(HttpStatus.BAD_REQUEST.value(), "report date is exits", HttpStatus.BAD_REQUEST.name());
+    public Object save(ReportRequest request, int groupId) {
+        try {
+            Optional<ReportEntity> entity = reportRepository.findByReportDateAndGroupId(DateUtils.formatDate(new Date()), groupId);
+            if (entity.isPresent()) {
+                return new SuccessResponse<>(HttpStatus.BAD_REQUEST.value(), "report date is exits", HttpStatus.BAD_REQUEST.name());
+            }
+            if (request.getRestNum() != request.getRestRequests().size()) {
+                return new SuccessResponse<>(HttpStatus.BAD_REQUEST.value(), "size rest not equal size effective", HttpStatus.BAD_REQUEST.name());
+            }
+            ReportEntity reportEntity = reportRepository.saveReport(request, groupId);
+            riceRepository.saveRice(request.getRiceRequests(), reportEntity.getId());
+            restRepository.saveRest(request.getRestRequests(), reportEntity.getId());
+            transferRepository.saveTransfer(request.getTransferRequests(), reportEntity.getId());
+            return reportDto(DateUtils.formatDate(reportEntity.getReportDate()), reportEntity.getGroupId());
+        } catch (Exception e) {
+            return new SuccessResponse<>(HttpStatus.BAD_GATEWAY.value(), "", HttpStatus.BAD_GATEWAY.name());
         }
-        if (request.getRestNum() != request.getRestRequests().size()) {
-            throw new ResourceNotFoundException(HttpStatus.BAD_REQUEST.value(), "size rest not equal size effective", HttpStatus.BAD_REQUEST.name());
-        }
-        ReportEntity reportEntity = reportRepository.saveReport(request, groupId);
-        riceRepository.saveRice(request.getRiceRequests(), reportEntity.getId());
-        restRepository.saveRest(request.getRestRequests(), reportEntity.getId());
-        transferRepository.saveTransfer(request.getTransferRequests(), reportEntity.getId());
-        return reportDto(DateUtils.formatDate(reportEntity.getReportDate()), reportEntity.getGroupId());
     }
 
     @Override
-    @Transactional
-    public ReportResponse update(ReportRequest request, int groupId) {
-        Optional<ReportEntity> entity = reportRepository.findByIdAndGroupId(request.getId(), groupId);
-        if (entity.isEmpty()) {
-            throw new ResourceNotFoundException(HttpStatus.BAD_REQUEST.value(), "report is not Exits", HttpStatus.BAD_REQUEST.name());
+    public Object update(ReportRequest request, int groupId) {
+        try {
+            Optional<ReportEntity> entity = reportRepository.findByIdAndGroupId(request.getId(), groupId);
+            if (entity.isEmpty()) {
+                return new SuccessResponse<>(HttpStatus.BAD_REQUEST.value(), "report is not Exits", HttpStatus.BAD_REQUEST.name());
+            }
+            ReportEntity reportEntity = reportRepository.updateReport(request, groupId);
+            riceRepository.updateRice(request.getRiceRequests(), reportEntity.getId());
+            restRepository.updateRest(request.getRestRequests(), reportEntity.getId());
+            transferRepository.updateTransfer(request.getTransferRequests(), reportEntity.getId(), groupId);
+            return reportDto(DateUtils.formatDate(reportEntity.getReportDate()), reportEntity.getGroupId());
+        } catch (Exception e) {
+            return new SuccessResponse<>(HttpStatus.BAD_GATEWAY.value(), "", HttpStatus.BAD_GATEWAY.name());
         }
-        ReportEntity reportEntity = reportRepository.updateReport(request, groupId);
-        riceRepository.updateRice(request.getRiceRequests(), reportEntity.getId());
-        restRepository.updateRest(request.getRestRequests(), reportEntity.getId());
-        transferRepository.updateTransfer(request.getTransferRequests(), reportEntity.getId(), groupId);
-        return reportDto(DateUtils.formatDate(reportEntity.getReportDate()), reportEntity.getGroupId());
     }
 }
