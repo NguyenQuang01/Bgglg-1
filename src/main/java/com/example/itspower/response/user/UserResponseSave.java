571f6a0429be8e06da0bbc73862adab007afba9e
AnhPD <Ducanh123>
2023-04-08 10:55:05 +0700

    refreshToken

--- src/main/java/com/example/itspower/response/user/UserResponseSave.java
+++ src/main/java/com/example/itspower/response/user/UserResponseSave.java
@@ -11,7 +11,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseSave {
-    private UserEntity user;
+    private String userName;
+    private boolean isEdit;
+    private boolean isReport;
+    private boolean isView;
+    private boolean isAdmin;
     private GroupEntity group;
     private UserGroupEntity userGroup;
+
+    public UserResponseSave(UserEntity user, GroupEntity group, UserGroupEntity userGroup) {
+        this.userName = user.getUserLogin();
+        this.isEdit = user.isEdit();
+        this.isReport = user.isReport();
+        this.isView = user.isView();
+        this.isAdmin = user.isAdmin();
+        this.group = group;
+        this.userGroup = userGroup;
+    }
 }
