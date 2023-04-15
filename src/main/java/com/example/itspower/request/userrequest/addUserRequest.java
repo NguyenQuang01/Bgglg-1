7a14e81a75e6beae68dc128ad3e2d55e99b06758
dai <nguyenvandaicd@gmail.com>
2023-04-15 23:01:20 +0700

    fix bug search view

---
+++ src/main/java/com/example/itspower/request/userrequest/addUserRequest.java
@@ -0,0 +1,10 @@
+package com.example.itspower.request.userrequest;
+
+import lombok.Data;
+@Data
+public class addUserRequest {
+    private Integer id;
+    private String name;
+    private String laborCode;
+    private Integer groupId;
+}
